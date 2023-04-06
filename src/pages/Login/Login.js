b6a4703c9df45d33ddafdbee73970665bc511241
Gabrielv2 <gabriel_f_amorim@estudante.sesisenai.org.br>
2023-04-05 21:23:18 -0300

    third commit

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,160 +1,163 @@
 import React from 'react';
-import { 
-  StyleSheet, 
-  TextInput, 
-  View, 
-  SafeAreaView, 
+import {
+  StyleSheet,
+  TextInput,
+  View,
+  SafeAreaView,
   Animated,
-  Text, 
+  Text,
   TouchableOpacity,
-  Keyboard 
-  } from 'react-native';
+  Keyboard
+} from 'react-native';
 import { useState, useEffect } from 'react';
 import { Ionicons } from '@expo/vector-icons'
 import { Image } from 'react-native';
 import Logo from '../../../assets/logo.png'
 
 
-export function Login({navigation}) {
+export function Login({ navigation }) {
 
-    const [inputEmail, setInputEmail] = useState('');
+  const [inputEmail, setInputEmail] = useState('');
 
-    const [inputPassword, setInputPassword] = useState('')
-  
-    const [showPassword, setShowPassoword] = useState(true)
-  
-    const [errorMessage, setErrorMessage] = useState('')
+  const [inputPassword, setInputPassword] = useState('')
 
-    function validateLogin(){
-        if(!inputEmail){
-          setErrorMessage('Digite seu e-mail')
-        }
-        else if(!inputPassword){
-          setErrorMessage('Digite sua senha')
-        } else{
-          fetch(API + '/users?cpf=' + inputEmail + '&password=' + inputPassword)
-          .then(async(response) => {
-            const data = await response.json()
-            if(data.length === 1) {
-              await AsyncStorage.setItem('@id_user',JSON.stringify(data[0].id))
-              navigation.navigate('User')
-            } else {
-              alert('Usuário não cadastrado')
-            }        
-          })
-          .catch(() => alert('Houve um erro ao tentar logar.'))
-        }
-      }
+  const [showPassword, setShowPassoword] = useState(true)
 
-      function navigateForSignUp() {
-        navigation.navigate('SignUp')
-      }
+  const [errorMessage, setErrorMessage] = useState('')
 
-      const [offset] = useState(new Animated.ValueXY({x: 0, y: 90}));
-      const [logo] = useState(new Animated.ValueXY({x: 300, y:300}))
-
-      useEffect(()=> {
-
-        keybordDidShowListener = Keyboard.addListener('keyboardDidShow', keyboardDidShow);
-        keybordDidHideListener = Keyboard.addListener('keyboardDidHide', keyboardDidHide);
-        
-        Animated.spring(offset.y, {
-          toValue: 0,
-          speed: 5,
-          bounciness: 10,
-          useNativeDriver: true
-        }).start();
-      }, []);
-
-      function keyboardDidShow()
-      {
-        Animated.parallel([
-        Animated.timing(logo.x, {
-          toValue: 100,
-          duration: 100,
-          useNativeDriver: false
-        }),
-        Animated.timing(logo.x, {
-          toValue: 300,
-          duration: 100,
-          useNativeDriver: false
-        })    
-        ]).start();
-      }
-
-      function keyboardDidHide()
-      {
-        Animated.parallel([
-          Animated.timing(logo.x, {
-            toValue: 300,
-            duration: 100,
-            useNativeDriver: false
-          }),
-          Animated.timing(logo.x, {
-            toValue: 300,
-            duration: 300,
-            useNativeDriver: false
-          })    
-          ]).start();
-      }
-
-    return (
-        
-    <SafeAreaView style={styles.background}> 
-         
-     <View style={styles.logo}>
-     <Animated.Image
-      source={require('../../../assets/logo.png')} 
-      style={{height: logo.y, width: logo.x}}
-    /> 
-      </View> 
-
-      <Animated.View 
-      style={[styles.container,
-      {
-        transform: [
-          {translateY: offset.y }
-        ]
-      } 
-      ]}
-      >
-        <TextInput
-          style={styles.input}
-          placeholder='Email'
-          keyboardType='number-pad'
-          value={inputEmail}
-          onChangeText={setInputEmail}
-        />
-        
-        <TextInput
-          style={styles.input}
-          placeholder='Senha'
-          secureTextEntry={showPassword}
-          value={inputPassword}
-          onChangeText={(password) => setInputPassword(password)}
-        />
-        <TouchableOpacity style={styles.icon} onPress={() => setShowPassoword(!showPassword)}>
-          {showPassword ?
-            <Ionicons name='eye' color='black' size={25} />
-            :
-            <Ionicons name='eye-off' color='black' size={25} />
+  function validateLogin() {
+    if (!inputEmail) {
+      setErrorMessage('Digite seu e-mail')
+    }
+    else if (!inputPassword) {
+      setErrorMessage('Digite sua senha')
+    } else {
+      fetch(API + '/users?cpf=' + inputEmail + '&password=' + inputPassword)
+        .then(async (response) => {
+          const data = await response.json()
+          if (data.length === 1) {
+            await AsyncStorage.setItem('@id_user', JSON.stringify(data[0].id))
+            navigation.navigate('User')
+          } else {
+            alert('Usuário não cadastrado')
           }
-        </TouchableOpacity>
-        
-      <TouchableOpacity onPress={navigateForSignUp}><Text>Esqueceu sua senha?</Text></TouchableOpacity>
-      <TouchableOpacity onPress={validateLogin} style={styles.button}><Text style={{fontSize: 15}} onPress={validateLogin}>Logar</Text></TouchableOpacity>
-      <TouchableOpacity onPress={navigateForSignUp}><Text>Abrir conta gratuita</Text></TouchableOpacity>
-          
-     
-      {
-        errorMessage!== '' && (
-          <View>
-            <Text>{errorMessage}</Text>
-          </View>
-        )        
-      }
+        })
+        .catch(() => alert('Houve um erro ao tentar logar.'))
+    }
+  }
 
-</Animated.View>
+  function navigateForSignUp() {
+    navigation.navigate('SignUp')
+  }
+
+  const [offset] = useState(new Animated.ValueXY({ x: 0, y: 90 }));
+  const [logo] = useState(new Animated.ValueXY({ x: 300, y: 300 }))
+
+  useEffect(() => {
+
+    keybordDidShowListener = Keyboard.addListener('keyboardDidShow', keyboardDidShow);
+    keybordDidHideListener = Keyboard.addListener('keyboardDidHide', keyboardDidHide);
+
+    Animated.spring(offset.y, {
+      toValue: 0,
+      speed: 5,
+      bounciness: 10,
+      useNativeDriver: true
+    }).start();
+  }, []);
+
+  function keyboardDidShow() {
+    Animated.parallel([
+      Animated.timing(logo.x, {
+        toValue: 100,
+        duration: 100,
+        useNativeDriver: false
+      }),
+      Animated.timing(logo.x, {
+        toValue: 300,
+        duration: 100,
+        useNativeDriver: false
+      })
+    ]).start();
+  }
+
+  function keyboardDidHide() {
+    Animated.parallel([
+      Animated.timing(logo.x, {
+        toValue: 300,
+        duration: 100,
+        useNativeDriver: false
+      }),
+      Animated.timing(logo.x, {
+        toValue: 300,
+        duration: 300,
+        useNativeDriver: false
+      })
+    ]).start();
+  }
+
+  return (
+
+    <SafeAreaView style={styles.background}>
+
+      <View style={styles.logo}>
+        <Animated.Image
+          source={require('../../../assets/logo.png')}
+          style={{ height: logo.y, width: logo.x }}
+        />
+      </View>
+
+      <Animated.View
+        style={[styles.container,
+        {
+          transform: [
+            { translateY: offset.y }
+          ]
+        }
+        ]}
+      >
+
+
+        <View style={styles.input}>
+          <TextInput  
+            placeholder='Email'
+            value={inputEmail}
+            onChangeText={setInputEmail}
+          />
+          </View>
+
+          <View style={styles.input}>
+            <TextInput
+              placeholder='Senha'
+              secureTextEntry={showPassword}
+              value={inputPassword}
+              onChangeText={(password) => setInputPassword(password)}
+            />
+            <View style={styles.icon}>
+              <TouchableOpacity onPress={() => setShowPassoword(!showPassword)}>
+                {showPassword ?
+                  <Ionicons name='eye' color='black' size={25} />
+                  :
+                  <Ionicons name='eye-off' color='black' size={25} />
+                }
+              </TouchableOpacity>
+            </View>
+          </View>
+
+          <TouchableOpacity onPress={navigateForSignUp}><Text>Esqueceu sua senha?</Text></TouchableOpacity>
+          <TouchableOpacity onPress={validateLogin} style={styles.button}><Text style={{ fontSize: 15 }} onPress={validateLogin}>Logar</Text></TouchableOpacity>
+          <TouchableOpacity onPress={navigateForSignUp}><Text>Abrir conta gratuita</Text></TouchableOpacity>
+
+
+          {
+            errorMessage !== '' && (
+              <View>
+                <Text>{errorMessage}</Text>
+              </View>
+            )
+          }
+
+      </Animated.View>
 
 
     </SafeAreaView>
@@ -169,26 +172,28 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  logo: {    
-   flex: 1, 
-   justifyContent: 'center'
+  logo: {
+    flex: 1,
+    justifyContent: 'center'
   },
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    width: '80%'
+    width: '90%'
 
   },
   input: {
     width: '90%',
     height: 50,
     padding: 8,
-    fontSize: 18, 
-    backgroundColor: '#FFF',
-    marginBottom: 10,
     fontSize: 18,
-    borderRadius: 10   
+    backgroundColor: '#FFF',
+    fontSize: 18,
+    borderRadius: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    margin: 10
   },
   button: {
     width: '90%',
@@ -197,5 +202,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     borderRadius: 10,
-  }
+  },
+  icon: {
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
+
 });
